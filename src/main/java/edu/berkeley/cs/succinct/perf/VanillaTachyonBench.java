f29ee95971eb6798492b4fe821bbd1e3fede29f5
Ujval Misra <ujval.misra@berkeley.edu>
2016-01-29 23:16:14 -0800

    changed warmup/cooldown constants

--- src/main/java/edu/berkeley/cs/succinct/perf/VanillaTachyonBench.java
+++ src/main/java/edu/berkeley/cs/succinct/perf/VanillaTachyonBench.java
@@ -24,11 +24,11 @@ public class VanillaTachyonBench {
     private static final String READ_TYPE = "NO_CACHE";
     private static final int MAX_QUERIES = 100000;
     private static final int WARMUP_QUERIES = 10000;
-    private static final int MAX_THR_EXT_QUERIES = 2000000000;
+    private static final int MAX_THR_EXT_QUERIES = 100000000;
 
-    private static final int WARMUP_TIME = 300; // seconds
-    private static final int COOLDOWN_TIME = 300; // seconds
-    private static final int MEASUREMENT_TIME = 600; // seconds
+    private static final int WARMUP_TIME = 180; // seconds
+    private static final int COOLDOWN_TIME = 180; // seconds
+    private static final int MEASUREMENT_TIME = 360; // seconds
 
     private TachyonFileSystem tfs;
     private ByteBuffer buf;
